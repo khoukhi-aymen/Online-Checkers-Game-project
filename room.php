b39d83ab378ca5461f45cbbefa407dd295c91e4e
Khoukhi Aymene <101263582+khoukhi-aymen@users.noreply.github.com>
2024-12-04 01:10:36 -0800

    Update room.php

--- room.php
+++ room.php
@@ -8,6 +8,7 @@ require('actions/roomAction.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css"/>
     <style>
     td,table,th{
       border_collapse: collapse; 
